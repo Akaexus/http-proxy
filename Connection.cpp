fe163566513ad9cba1f23d6249a04fcf461e376a
AlistarIsCow <62110503+AlistarIsCow@users.noreply.github.com>
2021-12-20 19:03:56 +0100

    sprzątanie cz 3

---
+++ Connection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by krzysztof on 20.12.2021.
+//
+
+#include "Connection.h"
---
+++ Connection.h
@@ -0,0 +1,14 @@
+//
+// Created by krzysztof on 20.12.2021.
+//
+
+#ifndef HTTP_PROXY_CONNECTION_H
+#define HTTP_PROXY_CONNECTION_H
+
+
+class Connection {
+
+};
+
+
+#endif //HTTP_PROXY_CONNECTION_H
